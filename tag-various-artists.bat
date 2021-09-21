157b0bae81144abc946e360c154876027d5876ac
David Craddock <contact@davidcraddock.net>
2021-09-21 03:40:19 +0100

    Fixed problems with Various Artists being appended to albumartist field
    Added a dependancy on metaflac.exe

--- tag-various-artists.bat
+++ tag-various-artists.bat
@@ -1,5 +1,18 @@
+REM -- Set 'Various Artists' field from a valid 'beets ls' query string
+REM
+REM -- Needs paths set to kid3-cli.exe windows 64 build from https://kid3.kde.org/
+REM -- and metaflac.exe from https://ftp.osuosl.org/pub/xiph/releases/flac/
+REM
+REM -- Remember to run 'beet update' after you are happy with the modifications
+REm -- and then 'beet move'
+
+set PATH=%PATH%;C:\Users\conta\Downloads\kid3-3.8.7-win32-x64\kid3-3.8.7-win32-x64
+set PATH=%PATH%;C:\Users\conta\Downloads\flac-1.3.2-win\flac-1.3.2-win\win64
+
 setlocal
 FOR /F "tokens=*" %%G IN ('beet ls -p %*') DO (
-  kid3-cli.exe -c "remove albumartist" -c "set albumartist 'Various Artists'" "%%G"
+  kid3-cli.exe -c "set albumartist ''" -c "set albumartist 'Various Artists'" "%%G"
+  metaflac.exe --remove-tag="album artist" "%%G"
+  metaflac.exe --set-tag="album artist=Various Artists" "%%G"
 )
 endlocal
