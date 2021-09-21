1b97bba119b8ee131ed1911b3cd5aa6d12a828db
David Craddock <contact@davidcraddock.net>
2021-09-21 04:06:47 +0100

    Comment typo

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
-# deploy scripts to my music directories, probably not useful for anyone but me
+REM Deploy scripts to my music directories, probably not useful for anyone but me
 
 copy .\download-and-sync.bat ..\music\bandcampincoming\
 copy .\tag-various-artists.bat ..\music\
