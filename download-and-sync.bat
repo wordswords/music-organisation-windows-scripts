4bbf546e60233df03303d45f27044cccb9fc770e
David Craddock <contact@davidcraddock.net>
2021-09-21 03:58:09 +0100

    Fully working now

--- download-and-sync.bat
+++ download-and-sync.bat
@@ -4,8 +4,9 @@ REM 3. Remember to change 'socialistsynthesis' to your own bandcamp ID!
 
 setlocal
 set jdkpath=C:\Program Files\Java\jdk-17
-set PATH=%PATH%;%jdkpath%
-setx JAVA_HOME=%jdkpath%
+set PATH=%jdkpath%\bin;%PATH%
+echo %PATH%
+setx JAVA_HOME %jdkpath%
 set JAVA_HOME=%jdkpath%
 java.exe -jar bandcamp-collection-downloader.jar -f=flac -j=1 -r=3 socialistsynthesis
 endlocal
