b2f687d9fe912a70615e14ffdf84f5f99ac48bdd
David Craddock <contact@davidcraddock.net>
2021-09-21 03:44:08 +0100

    Documented and simplified bandcamp downloader bat file

--- download-and-sync.bat
+++ download-and-sync.bat
@@ -1,4 +1,9 @@
-:: %JAVA_HOME% = C:\Program Files\Java\jdk-17
+REM 1. Needs JDK > 17 path set, latest JDK is recommended
+REM 2. Needs https://github.com/Ezwen/bandcamp-collection-downloader jar in same directory
+REM 3. Remember to change 'socialistsynthesis' to your own bandcamp ID!
+
 setx JAVA_HOME "C:\Program Files\Java\jdk-17"
 set JAVA_HOME "C:\Program Files\Java\jdk-17"
 "C:\Program Files\Java\jdk-17\bin\java.exe" -jar bandcamp-collection-downloader.jar -f=flac -j=1 -r=3 socialistsynthesis
+
+
