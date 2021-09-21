a12071b9f3da181eb7bc2278e3dc7708c79b7949
David Craddock <wordswords@users.noreply.github.com>
2021-09-21 19:00:01 +0100

    Update tag-various-artists.bat

--- tag-various-artists.bat
+++ tag-various-artists.bat
@@ -8,7 +8,6 @@ REM -- Remember to run 'beet update' after you are happy with the modifications
 REm -- and then 'beet move'
 
 setlocal
-
 set PATH=%PATH%;C:\Users\conta\Downloads\kid3-3.8.7-win32-x64\kid3-3.8.7-win32-x64
 set PATH=%PATH%;C:\Users\conta\Downloads\flac-1.3.2-win\flac-1.3.2-win\win64
 
