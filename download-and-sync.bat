84c70dee7832b51c74a1c26b98df8f96bba10d93
David Craddock <wordswords@users.noreply.github.com>
2021-09-21 03:54:06 +0100

    Tidied up a bit with my newfound batchfile skills

--- download-and-sync.bat
+++ download-and-sync.bat
@@ -2,8 +2,11 @@ REM 1. Needs JDK > 17 path set, latest JDK is recommended
 REM 2. Needs https://github.com/Ezwen/bandcamp-collection-downloader jar in same directory
 REM 3. Remember to change 'socialistsynthesis' to your own bandcamp ID!
 
-setx JAVA_HOME "C:\Program Files\Java\jdk-17"
-set JAVA_HOME "C:\Program Files\Java\jdk-17"
-"C:\Program Files\Java\jdk-17\bin\java.exe" -jar bandcamp-collection-downloader.jar -f=flac -j=1 -r=3 socialistsynthesis
-
+setlocal
+set jdkpath=C:\Program Files\Java\jdk-17
+set PATH=%PATH%;%jdkpath%
+setx JAVA_HOME=%jdkpath%
+set JAVA_HOME=%jdkpath%
+java.exe -jar bandcamp-collection-downloader.jar -f=flac -j=1 -r=3 socialistsynthesis
+endlocal
 
