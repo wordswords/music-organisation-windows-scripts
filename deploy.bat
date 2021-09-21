7b276fc0cef2ab598767e08ad18d79bcbd1d86a1
David Craddock <contact@davidcraddock.net>
2021-09-21 04:01:22 +0100

    Added simple deploy script for me

---
+++ deploy.bat
@@ -0,0 +1,6 @@
+# deploy scripts to my music directories, probably not useful for anyone but me
+
+copy .\download-and-sync.bat ..\music\bandcampincoming\
+copy .\tag-various-artists.bat ..\music\
+
+
