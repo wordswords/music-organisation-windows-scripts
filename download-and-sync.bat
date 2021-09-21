34ad4463a315ff2769c1851cf5945f55ceb05b39
David Craddock <contact@davidcraddock.net>
2021-09-21 04:05:12 +0100

    A bit more documentation

--- download-and-sync.bat
+++ download-and-sync.bat
@@ -1,3 +1,7 @@
+REM Download all your purchased FLAC files from Bandcamp
+REM 
+REM It maintains an index and will not redownload FLACs unless you delete those items in the index
+REM
 REM 1. Needs JDK > 17 path set, latest JDK is recommended
 REM 2. Needs https://github.com/Ezwen/bandcamp-collection-downloader jar in same directory
 REM 3. Remember to change 'socialistsynthesis' to your own bandcamp ID!
